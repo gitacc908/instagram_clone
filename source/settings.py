8fbb81be913c98e10f5d94dcbd86854d8d25208d
leap of faith <dylan9803@mail.ru>
2022-03-05 00:37:08 +0600

    template updates/req updates

--- source/settings.py
+++ source/settings.py
@@ -32,6 +32,8 @@ INSTALLED_APPS = [
     'channels',
     'cloudinary',
     'cloudinary_storage',
+    'django_celery_beat',
+    'django_celery_results',
 
     # custom apps
     'apps.users',
@@ -179,14 +181,6 @@ LOGIN_REDIRECT_URL = 'index'
 LOGOUT_REDIRECT_URL = 'sign-in'
 LOGIN_URL = 'sign-in'
 
-# CHANNEL_LAYERS = {
-#     'default':{
-#         'BACKEND': 'channels_redis.core.RedisChannelLayer',
-#         'CONFIG': {
-#             'hosts': [('127.0.0.1', 6379)], 
-#         },
-#     },
-# }
 CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
@@ -196,12 +190,12 @@ CHANNEL_LAYERS = {
     },
 }
 # CELERY SETTINGS
-CELERY_BROKER_URL = os.environ['REDIS_URL']
-#CELERY_BROKER_URL = 'redis://127.0.0.1:6379'
-CELERY_RESULT_BACKEND = 'django-db'
-CELERY_ACCEPT_CONTENT = ['application/json']
-CELERY_RESULT_SERIALIZER = 'json'
-CELERY_TASK_SELERLIZER = 'json'
-CELERY_TIMEZONE = 'Asia/Kolkata'
+# CELERY_BROKER_URL = os.environ['REDIS_URL']
+# #CELERY_BROKER_URL = 'redis://127.0.0.1:6379'
+# CELERY_RESULT_BACKEND = 'django-db'
+# CELERY_ACCEPT_CONTENT = ['application/json']
+# CELERY_RESULT_SERIALIZER = 'json'
+# CELERY_TASK_SELERLIZER = 'json'
+# CELERY_TIMEZONE = 'Asia/Bishkek'
 
-CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
+# CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
